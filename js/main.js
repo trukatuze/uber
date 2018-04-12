9675fe893a8f890cef74a231f80b8cf5e219d340
trukatuze <trukatuze@gmail.com>
2018-04-12 16:01:57 +0300

    scroll

--- js/main.js
+++ js/main.js
@@ -21,4 +21,11 @@ $(function(){
         }
         });
     });
+
+    document.body.onkeydown = function(e){
+        e = e || window.event;
+        var c = e.keyCode;
+//Убирает эвент на стрелках, на pageDown, PageUp, Home, End
+        if(c>36 && c<41 || c>32 && c<37) return false;
+    }
 });
