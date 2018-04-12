5e26459e88d724b0952b2fe7fbb23734415a089d
trukatuze <trukatuze@gmail.com>
2018-04-12 15:09:27 +0300

    responsiv

--- js/main.js
+++ js/main.js
@@ -1,33 +1,7 @@
 $(document).ready(function() {
-    $('.about').submit(function (e) {
-        e.preventDefault();
-        var data = $(this).serialize();
-        $.ajax({
-            type: "POST",
-            url:"test.php",
-            data:data,
-            success:function (about){
-                $('.about').html(about);
-            }
-        });
-    });
-});
-$('.ok').on('click', (function (e) {
-    e.preventDefault();
-}));
-$('#home').on('click', (function (e) {
-    e.preventDefault();
-}));
-$('#about').on('click', (function (e) {
-    e.preventDefault();
-}));
-$('#contact').on('click', (function (e) {
-    e.preventDefault();
-}));
-
 $(function(){
     var vid = document.getElementById('bgvid');
-    var pause = $('#info-block button');
+    var pause = $('#info-block #pause');
 
     vid.onEnded = function(){
         this.pause();
@@ -45,5 +19,6 @@ $(function(){
             vid.pause();
             $(this).html('Paused');
         }
+        });
     });
 });
