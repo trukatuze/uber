bce5030d10b0848d7137c4c98cffcf788c21f1fc
trukatuze <trukatuze@gmail.com>
2018-04-12 16:03:38 +0300

    scroll1

--- js/main.js
+++ js/main.js
@@ -21,11 +21,4 @@ $(function(){
         }
         });
     });
-
-    document.body.onkeydown = function(e){
-        e = e || window.event;
-        var c = e.keyCode;
-//Убирает эвент на стрелках, на pageDown, PageUp, Home, End
-        if(c>36 && c<41 || c>32 && c<37) return false;
-    }
 });
